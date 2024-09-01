af141f7e30eb404821dcc9d12bbbc4741de8033e
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-09-01 12:10:12 +0500

    Event Listner Practice

---
+++ main.js
@@ -0,0 +1,38 @@
+// const doSomething = ()=>{
+//     alert("Doing somethig");
+// };
+// h2.addEventListener("click",doSomething,false);
+// h2.removeEventListener("click",doSomething,false);
+
+// h2.addEventListener("click" , (event)=>{
+//     console.log(event.target);
+//     event.target.textContent = "Hello"
+
+// });
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    console.log("readyState = Completed");
+    initApp();
+  }
+});
+let initApp = () => {
+  const view = document.querySelector("#view2");
+  const div = view.querySelector("div");
+  const h2 = div.querySelector("h2");
+
+  view.addEventListener("click", (event) => {
+    view.classList.toggle("purple");
+    view.classList.toggle("darkblue");
+  });
+  div.addEventListener("click", (event) => {
+    div.classList.toggle("blue");
+    div.classList.toggle("black");
+  });
+  h2.addEventListener("click", (event) => {
+    const myText = event.target.textContent;
+      myText === "My 2nd View"?event.target.textContent ="Cliked":
+      event.target.textContent = "My 2nd View";
+
+  });
+};
